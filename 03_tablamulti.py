48a4810c65e0f671d02be863658a5c303226a6f7
AntonioCh <antoniochavezcampos94@gmail.com>
2020-11-21 14:05:27 -0600

    Add sample about range and home project

---
+++ 03_tablamulti.py
@@ -0,0 +1,20 @@
+#Script que calcula la tabla de multiplicar de un numero
+
+#Solicitar numero al usuario por consola
+numero = input('que numero quieres multiplicar?')
+
+#Dato ingresado por usuario es una cadena 'str' 
+#Debe convertir a numero int para poder multiplicar 
+numero = int(numero)
+
+for n in range(10):
+    r = numero * (n + 1)
+    print(r)
+
+#A continuacion se muestra la tabla de multiplicar del numero <numero>
+#----------
+# 8 * 1 = 8
+# 8 * 2 = 16
+# ...
+
+ 
