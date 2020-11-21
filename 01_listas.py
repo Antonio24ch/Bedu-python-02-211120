c1c59c1b2d476d7c4adb54a9be5b1c0c6b388671
AntonioCh <antoniochavezcampos94@gmail.com>
2020-11-21 13:24:45 -0600

    Added file to explain lists in python

---
+++ 01_listas.py
@@ -0,0 +1,16 @@
+#Archivo explicando listas
+
+numero = [23,89,127,1289,8912]
+
+#Para agregar un elemento a una lista ocupamos append
+numero_extra = 33
+numero_extra1 = 1223
+numero_extra2 = 89
+
+numero.append(numero_extra)
+
+print(numero)
+
+numero.pop()
+
+print(numero)
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ### SESIÓN 02
 
 Ejemplos realizados de mi clase número 2 en Bedu.org 
+ 
